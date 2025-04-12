7b9b2cf607ce99f4f3a1828e94bec96bac48be47
JMTC-dev <jmtcdev@gmail.com>
2025-04-12 14:06:04 +1000

    fix: fix re-render of board data by setting state to board data

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,21 @@ const row = 6;
 const col = 7;
 const winScore = 4;
 
-function App() {
-  const [boardData, setBoardData] = useState<Board>();
-  const initBoard = () => {
-    let newBoard: Board = [];
-    for (let r = 0; r < row; r++) {
-      for (let c = 0; c < col; c++) {
-        if (!newBoard[r]) {
-          newBoard[r] = [];
-        }
-        newBoard[r].push(0);
+const initBoard = () => {
+  let newBoard: Board = [];
+  for (let r = 0; r < row; r++) {
+    for (let c = 0; c < col; c++) {
+      if (!newBoard[r]) {
+        newBoard[r] = [];
       }
+      newBoard[r].push(0);
     }
-    setBoardData(newBoard);
-  };
-  initBoard();
+  }
+  return newBoard;
+};
 
+function App() {
+  const [boardData, setBoardData] = useState<Board>(() => initBoard());
   return (
     <>
       <h1>Connect 4</h1>
